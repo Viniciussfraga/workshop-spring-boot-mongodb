e05924708578c905842a98f0a1c806fca4e043e4
Vinicius Fraga <viniciusfrg@outlook.com.br>
2022-10-11 10:05:06 -0300

    Insercao de usuario com POST

--- src/main/java/com/viniciusfraga/workshopmongo/services/UserService.java
+++ src/main/java/com/viniciusfraga/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.viniciusfraga.workshopmongo.domain.User;
+import com.viniciusfraga.workshopmongo.dto.UserDTO;
 import com.viniciusfraga.workshopmongo.repository.UserRepository;
 import com.viniciusfraga.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -25,4 +26,10 @@ public class UserService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
 }
